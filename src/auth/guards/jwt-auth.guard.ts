234753b965bf6f8bdf75f694d8d6c82a72a52c4b
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2026-01-02 09:46:32 +0800

    authguard

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,10 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,  UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   handleRequest(err, client, info) {
     console.log('JwtAuthGuard handleRequest: ', { err, client, info });
+
+    if (err || !client) {
+      throw err || new UnauthorizedException('Invalid or missing token');
+    }
 
     return client; // must return the client for Nest to attach to req.client
   }
